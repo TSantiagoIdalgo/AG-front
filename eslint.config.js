b1a1a2f0717a0b9002fb7ba5d7ccd62ab2381188
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-11-19 18:07:16 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.all,
+  ...tseslint.configs.strict,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // eslint-disable-next-line no-magic-numbers
+      "indent": ["error", 2],
+      "semi": ["error", "always"]
+    }
+  }
+];
